a94faed0e7f948e5743446c1ca79d4b835fe1488
nidal <nidalcheruvadi@gmail.com>
2025-05-19 17:08:14 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿namespace StringTask
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var ab = Answer.Product("123");
+            Console.WriteLine(ab);
+
+        }
+
+    }
+
+    public class Answer
+    {
+        public static int Product(string a)
+        {
+            int b = 1;
+       
+            foreach (var item in a)
+            {
+                int c = Convert.ToInt32(item);
+
+                int d =  b * c;
+
+                b += d;
+
+
+            }
+            Console.WriteLine(b);
+
+            return b;
+        }
+    } }
